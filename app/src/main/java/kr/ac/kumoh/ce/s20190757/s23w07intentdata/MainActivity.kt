ed03dfce3e27fa1079cce15dea653af03d973507
UnKnown <huroku1591@naver.com>
2023-10-19 13:49:05 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20190757/s23w07intentdata/MainActivity.kt
@@ -0,0 +1,15 @@
+package kr.ac.kumoh.ce.s20190757.s23w07intentdata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.ce.s20190757.s23w07intentdata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var main: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        main = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(main.root)
+    }
+}
