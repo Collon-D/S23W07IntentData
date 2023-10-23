519466a957a931b2f33cf0e1d96c56dfb9bd5218
UnKnown <huroku1591@naver.com>
2023-10-23 14:50:09 +0900

    Updata

--- app/src/main/java/kr/ac/kumoh/ce/s20190757/s23w07intentdata/ImageActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20190757/s23w07intentdata/ImageActivity.kt
@@ -7,6 +7,13 @@ import android.view.LayoutInflater
 import kr.ac.kumoh.ce.s20190757.s23w07intentdata.databinding.ActivityImageBinding
 
 class ImageActivity : AppCompatActivity() {
+    companion object {
+        const val IMAGE_RESULT = "image result"
+        const val LIKE = 100
+        const val DISLIKE = 101
+        const val NONE = 0
+    }
+
     private lateinit var main: ActivityImageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,8 +23,8 @@ class ImageActivity : AppCompatActivity() {
 
         Log.i("Image!!", intent.getStringExtra(MainActivity.KEY_NAME) ?: "없음")
         val res = when (intent.getStringExtra(MainActivity.KEY_NAME)) {
-            "newjeans1" -> R.drawable.newjeans1
-            "newjeans2" -> R.drawable.newjeans2
+            MainActivity.IMAGE_NEWJEANS1 -> R.drawable.newjeans1
+            MainActivity.IMAGE_NEWJEANS2 -> R.drawable.newjeans2
             else -> R.drawable.ic_launcher_foreground
         }
         main.image.setImageResource(res)
--- app/src/main/java/kr/ac/kumoh/ce/s20190757/s23w07intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20190757/s23w07intentdata/MainActivity.kt
@@ -1,36 +1,67 @@
 package kr.ac.kumoh.ce.s20190757.s23w07intentdata
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import kr.ac.kumoh.ce.s20190757.s23w07intentdata.ImageActivity.Companion.IMAGE_RESULT
+import kr.ac.kumoh.ce.s20190757.s23w07intentdata.ImageActivity.Companion.NONE
 import kr.ac.kumoh.ce.s20190757.s23w07intentdata.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     companion object {
         const val KEY_NAME = "Singer"
+        const val IMAGE_NEWJEANS1 = "newjeans1"
+        const val IMAGE_NEWJEANS2 = "newjeans2"
     }
     private lateinit var main: ActivityMainBinding
+    private val startForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode != Activity.RESULT_OK)
+            return@registerForActivityResult
+
+        val result = it.data?.getIntExtra(IMAGE_RESULT, NONE)
+
+        val str = when (result) {
+            ImageActivity.LIKE -> "좋아요"
+            ImageActivity.DISLIKE -> "싫어요"
+            else -> "error"
+        }
+
+        Log.i("result", str)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main = ActivityMainBinding.inflate(layoutInflater)
         setContentView(main.root)
 
-        main.btnMusic1.setOnClickListener{
-            Toast.makeText(this, "NewJeans1", Toast.LENGTH_SHORT).show()
+        main.btnMusic1.setOnClickListener(this)
+        main.btnMusic2.setOnClickListener(this)
+    }
 
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME, "newjeans1")
-            startActivity(intent)
+    override fun onClick(v: View?) {
+        val value = when (v?.id) {
+            main.btnMusic1.id -> {
+                Toast.makeText(this, "NewJeans1", Toast.LENGTH_SHORT).show()
+                IMAGE_NEWJEANS1
+            }
+            main.btnMusic2.id -> {
+                Toast.makeText(this, "NewJeans2", Toast.LENGTH_SHORT).show()
+                IMAGE_NEWJEANS2
+            }
+            else -> return
         }
 
-        main.btnMusic2.setOnClickListener{
-            Toast.makeText(this, "NewJeans2", Toast.LENGTH_SHORT).show()
-
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra(KEY_NAME, "newjeans2")
-            startActivity(intent)
-        }
+        val intent = Intent(this, ImageActivity::class.java)
+        intent.putExtra(KEY_NAME, value)
+        //startActivity(intent)
+        startForResult.launch(intent)
     }
 }
